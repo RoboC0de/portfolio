c13a723079bc7d77b4dc493c1314e1096ae733be
Ahmadi <s.ahmadi@student.tue.nl>
2019-08-29 09:45:00 +0200

    Updates

---
+++ precache-manifest.c87db263cf92a93b442c6b7f4a252890.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f192b384ccc094343983dc68acc701",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76a953b81c3b783e12eb",
+    "url": "/portfolio/static/css/main.11d69278.chunk.css"
+  },
+  {
+    "revision": "6264e2c97aab7928671a",
+    "url": "/portfolio/static/js/2.bcc93d2a.chunk.js"
+  },
+  {
+    "revision": "76a953b81c3b783e12eb",
+    "url": "/portfolio/static/js/main.bf452f46.chunk.js"
+  },
+  {
+    "revision": "d748eba46e290608e30b",
+    "url": "/portfolio/static/js/runtime~main.efe3dbaf.js"
+  },
+  {
+    "revision": "d47fdf31b55c594b083e2c8e4be4af51",
+    "url": "/portfolio/static/media/Destination1.d47fdf31.png"
+  },
+  {
+    "revision": "8a03970de37c347c4166d8743a7f77ad",
+    "url": "/portfolio/static/media/Destination2.8a03970d.png"
+  },
+  {
+    "revision": "83b9987702d7f1cc8f26378cf25f7b3b",
+    "url": "/portfolio/static/media/LandingPageImg1.83b99877.jpg"
+  },
+  {
+    "revision": "7b1dd22148a827f84a884176f10be503",
+    "url": "/portfolio/static/media/Playlistify1.7b1dd221.png"
+  },
+  {
+    "revision": "e1fa53d970022665854666faa908fb8b",
+    "url": "/portfolio/static/media/Playlistify2.e1fa53d9.png"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/portfolio/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/portfolio/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/portfolio/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/portfolio/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/portfolio/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/portfolio/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "9677fdf5978b6fccfb2129c9f3c99bd8",
+    "url": "/portfolio/static/media/Ta-aam1.9677fdf5.png"
+  },
+  {
+    "revision": "2836e1ac1a406ae421bf34e248032515",
+    "url": "/portfolio/static/media/Ta-aam2.2836e1ac.png"
+  },
+  {
+    "revision": "0cdbfd084192a2a859fe89dce56e1348",
+    "url": "/portfolio/static/media/github.0cdbfd08.png"
+  },
+  {
+    "revision": "c37c174876d22ad66cb07d0ef2bf0166",
+    "url": "/portfolio/static/media/side2-new.c37c1748.png"
+  }
+]);
